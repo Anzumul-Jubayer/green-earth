2bcb46b5db857a689f9b76455bd380b1a347057f
S.M ANZUMUL JUBAYER <anzumuljubayer222@gmail.com>
2025-09-08 14:36:20 +0600

    cart-functionality

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/categories")
     .then((res) => res.json())
     .then((data) => showCategories(data.categories))
-    .catch((err) => alert('❌ failed'));
+    .catch((err) => alert("❌ failed"));
 };
 
 // Show categories
@@ -30,7 +30,9 @@ const showCategories = (categories) => {
   });
 
   const allTrees = document.getElementById("all-trees");
-  if (allTrees) allTrees.classList.add("bg-green-700", "text-white");
+  if (allTrees) {
+    allTrees.classList.add("bg-green-700", "text-white");
+  }
 };
 
 // all plants
@@ -39,7 +41,7 @@ const loadAllPlants = () => {
   fetch("https://openapi.programming-hero.com/api/plants")
     .then((res) => res.json())
     .then((data) => showTreeCategories(data.plants))
-    .catch((err) => alert('❌ failed'));
+    .catch((err) => alert("❌ failed"));
 };
 
 // category plants
@@ -48,7 +50,7 @@ const loadPlantsByCategory = (id) => {
   fetch(`https://openapi.programming-hero.com/api/category/${id}`)
     .then((res) => res.json())
     .then((data) => showTreeCategories(data.plants))
-    .catch((err) => alert('❌ failed'));
+    .catch((err) => alert("❌ failed"));
 };
 
 // Show plants
@@ -58,10 +60,10 @@ const showTreeCategories = (plants) => {
     cardDiv.innerHTML += `
       <div class="card bg-base-100 p-3 shadow-sm">
         <figure>
-          <img src="${plant.image}" class="w-full h-55 object-cover" />
+          <img src="${plant.image}" class="w-full h-56 object-cover" />
         </figure>
         <div class="card-body px-2">
-          <h2 onclick="loadTreeDetails(${plant.id})" class="card-title">${plant.name}</h2>
+          <h2  class="card-title"> <span onclick="loadTreeDetails(${plant.id})" class="cursor-pointer"> ${plant.name}</span> </h2>
           <p>${plant.description}</p>
           <div class="flex justify-between items-center">
             <div class="bg-[#dcfce7] text-green-700 font-semibold p-2 rounded-2xl">
@@ -82,9 +84,10 @@ const showTreeCategories = (plants) => {
 
 categoriesContainer.addEventListener("click", (event) => {
   if (event.target.tagName === "A") {
-    categoriesContainer.querySelectorAll("a").forEach((a) => {
+    const selectorA = categoriesContainer.querySelectorAll("a");
+    for (let a of selectorA) {
       a.classList.remove("bg-green-700", "text-white");
-    });
+    }
     event.target.classList.add("bg-green-700", "text-white");
     const id = event.target.getAttribute("id");
     if (id === "all-trees") {
@@ -103,7 +106,7 @@ const loadTreeDetails = (id) => {
     .then((data) => {
       showTreeDetails(data.plants);
     })
-    .catch((err) => alert('❌ failed'));
+    .catch((err) => alert("❌ failed"));
 };
 
 const showTreeDetails = (plant) => {
@@ -119,40 +122,53 @@ const showTreeDetails = (plant) => {
         <button class="btn">Close</button>
       </form>
     </div>
-  `;  
+  `;
   document.getElementById("tree-details").showModal();
 };
-
 
 // cart functionality
 let total = 0;
 let cartItems = {};
-cardDiv.addEventListener("click", (e) => {
-  if (!e.target.classList.contains("btn")) return;
-  const card = e.target.closest(".card");
-  const name = card.querySelector(".card-title").innerText;
-  const price = parseInt(card.querySelector("h3").innerText.split(" ").pop());
+cardDiv.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    const getTitle = event.target.parentNode.parentNode.children[0].innerText;
+    const getPrice = event.target.parentNode.parentNode.children[2].children[1].innerText;
+    const priceNumber = parseInt(getPrice);
+    if (!cartItems[getTitle]) {
+      const div = document.createElement("div");
+      div.innerHTML = `
+      <div class="flex justify-between items-center bg-green-100 p-3 rounded-lg my-3">
+            <div>
+              <h2 class="font-semibold">${getTitle}</h2>
+              <p class="text-gray-400">${getPrice} x 1</p>
+            </div>
+            <h2>
+             <span class="del-btn cursor-pointer"> ❌ </span>
+            </h2>
+           </div>
+     `;
+      cartDiv.appendChild(div);
 
-  if (!cartItems[name]) {
-    const div = document.createElement("div");
-    div.className =
-      "flex justify-between items-center bg-[#F0FDF4] p-3 my-3 rounded-lg";
-    div.innerHTML = `<div><h2 class="font-bold mb-2">${name}</h2><p class="text-gray-400"> ${price} x 0.00</p></div><div><h2 class="remove-btn cursor-pointer">❌</h2></div>`;
-    div.querySelector(".remove-btn").onclick = () => {
-      total -= cartItems[name].price * cartItems[name].count;
-      div.remove();
-      delete cartItems[name];
-      document.getElementById("total-price").innerText = `${total}`;
-    };
-    cartDiv.appendChild(div);
-    cartItems[name] = { count: 0, price, element: div };
+      div.querySelector(".del-btn").addEventListener("click", () => {
+        total -= priceNumber * cartItems[getTitle].count;
+        div.innerHTML = "";
+        delete cartItems[getTitle];
+        document.getElementById('total-value').innerText = total;
+      });
+      cartItems[getTitle]={ count:1,price:priceNumber,element:div}
+      
+    } else{
+      cartItems[getTitle].count+=1
+      const p=cartItems[getTitle].element.querySelector('p')
+      p.innerText = `${getPrice} x ${cartItems[getTitle].count}`;
+    }
+
+  total += priceNumber
+ document.getElementById('total-value').innerText= total
   }
-
-  cartItems[name].count++;
-  total += price;
-  cartItems[name].element.querySelector("p").innerHTML = `<i class="fa-solid fa-bangladeshi-taka-sign"></i> ${price} x ${cartItems[name].count}`;
-  document.getElementById("total-price").innerText = `${total}`;
 });
+
+
 
 //function call
 loadCategories();
